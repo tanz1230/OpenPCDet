707f812dafd922fdc9ada132954014812671c2cd
tanz1230 <zhu1230@purdue.edu>
2025-05-01 02:30:18 -0400

    minor update

--- tools/test.py
+++ tools/test.py
@@ -199,6 +199,7 @@ def main():
     )
 
     model = build_network(model_cfg=cfg.MODEL, num_class=len(cfg.CLASS_NAMES), dataset=test_set)
+    print("#############model built successfully")
     with torch.no_grad():
         if args.eval_all:
             repeat_eval_ckpt(model, test_loader, args, eval_output_dir, logger, ckpt_dir, dist_test=dist_test)
